d002c98510cd3e9d5d846bf3564cccfc10721ba2
andrewepeter <64112518+andrewepeter@users.noreply.github.com>
2025-01-04 21:20:21 -0500

    adding new files

---
+++ amplify/backend.ts
@@ -0,0 +1,9 @@
+import { defineBackend } from '@aws-amplify/backend';
+//import { api } from './backend/api/validateWordApi';
+import { auth } from './auth/resource';
+import { data } from './data/resource';
+
+defineBackend({
+  auth,
+  data,
+});
