d9b00e2370c5f169af617961476e91c0c38dcd3d
andrewepeter <64112518+andrewepeter@users.noreply.github.com>
2025-01-04 21:55:13 -0500

    test

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,4 +1,4 @@
-//import { defineBackend } from '@aws-amplify/backend';
+import { defineBackend } from '@aws-amplify/backend';
 //import { api } from './backend/api/validateWordApi';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
