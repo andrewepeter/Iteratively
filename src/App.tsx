bb7f2d6f68d47ea5d8a867a148e08053aca988f8
andrewepeter <64112518+andrewepeter@users.noreply.github.com>
2025-01-13 23:41:45 -0500

    fixed .env stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import HowToPlay from './HowToPlay';
 import { fetchAuthSession } from 'aws-amplify/auth';
+import { useAuthenticator } from '@aws-amplify/ui-react';
 
 function App() {
   const [word, setWord] = useState('');
@@ -18,6 +19,13 @@ function App() {
   const [comboLevel, setComboLevel] = useState(0);
   const [lastWordTime, setLastWordTime] = useState<number | null>(null);
   const [comboMessage, setComboMessage] = useState<string | null>(null);
+  const [leaderboard, setLeaderboard] = useState<LeaderboardEntry[]>([]);
+  const [showLeaderboard, setShowLeaderboard] = useState(false);
+  const [userTag, setUserTag] = useState('');
+  const [showSubmitForm, setShowSubmitForm] = useState(false);
+  const [showInstructions, setShowInstructions] = useState(false); // State to manage instructions visibility
+
+
   //const awsRegion = "ca-central-1";
   //const [leaderboardTable, setLeaderboardTable] = useState<string | null>(null);
 
@@ -25,11 +33,9 @@ function App() {
     id: string;
     username: string;
     score: number;
+    rank: number;
   }
-
-  const [leaderboard, setLeaderboard] = useState<LeaderboardEntry[]>([]);
-  const [showLeaderboard, setShowLeaderboard] = useState(false);
-
+  const { user } = useAuthenticator();
   const wordApiUrl = 'https://7jnx9golvc.execute-api.ca-central-1.amazonaws.com/prod/word';
   const LBApiUrl = 'https://uceb7mx731.execute-api.ca-central-1.amazonaws.com/prod/LB';
   const genWordUrl = 'https://dns37gpcu9.execute-api.ca-central-1.amazonaws.com/prod/genWord'
@@ -137,12 +143,11 @@ function App() {
       setWord('');
       setUserInput('');
       resetCombo();
+      setShowSubmitForm(true);
     }
   }, [timeLeft, word, isStarted, isLoading]);
 
-  const handleStart = () => {
-    setIsStarted(true);
-    setTimeLeft(60);
+  const handleStart = async () => {
     setError(null);
     setScore(0);
     setChainLength(0);
@@ -152,7 +157,9 @@ function App() {
     setComboMessage(null); // Clear combo message
     setWordList([]); // Clear the word list when starting a new game
     inputRef.current?.focus();
-    generateWord();
+    await generateWord();
+    setIsStarted(true);
+    setTimeLeft(60); // Start the timer after the word is generated
     //sounds.go.play();
   };
 
@@ -162,41 +169,48 @@ function App() {
   };
 
   const handleLeaderBoard = async () => {
-    try {
-      const session = await fetchAuthSession();
-      const token = session.tokens?.idToken?.toString();
-      console.log(token)
-      const headers: HeadersInit = {
-        'Content-Type': 'application/json',
-        'Authorization': String(token), // Include the token in the Authorization header
-      };
+    setShowLeaderboard((prev) => !prev); // Toggle the visibility of the leaderboard
+    if (!showLeaderboard) { // Only fetch the leaderboard if it is not currently visible
+      try {
+        const session = await fetchAuthSession();
+        const token = session.tokens?.idToken?.toString();
+        console.log(token);
+        const headers: HeadersInit = {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`, // Include the token in the Authorization header
+        };
 
-      const response = await fetch(LBApiUrl, {
-        method: 'GET',
-        headers: headers,
-      });
+        const response = await fetch(LBApiUrl, {
+          method: 'GET',
+          headers: headers,
+        });
 
-      if (response.ok) {
-        const responseText = await response.text();
-        console.log(responseText); // Log the response text for debugging
-        if (responseText) {
-          const data = JSON.parse(responseText);
-          setLeaderboard(data);
-          setShowLeaderboard(true);
+        if (response.ok) {
+          const responseText = await response.text();
+          console.log(responseText); // Log the response text for debugging
+          if (responseText) {
+            const data = JSON.parse(responseText);
+            const leaderboardData = data.leaderboard.map((entry: any, index: number) => ({
+              id: entry.Name, // Assuming 'Name' is unique and can be used as 'id'
+              username: entry.Tag || "Anonymous", // Use 'Tag' if available, otherwise use 'Anonymous'
+              score: entry.Score || 0, // Default to 0 if 'Score' is missing
+              rank: index + 1 // Add rank based on index
+            }));
+            setLeaderboard(leaderboardData);
+          } else {
+            setError("Empty response from server");
+          }
         } else {
-          setError("Empty response from server");
+          const errorText = await response.text();
+          console.error('Error response text:', errorText); // Log the error response text
+          setError("Failed to fetch leaderboard");
         }
-      } else {
-        const errorText = await response.text();
-        console.error('Error response text:', errorText); // Log the error response text
-        setError("Failed to fetch leaderboard");
+      } catch (error) {
+        console.error('Error fetching leaderboard:', error);
+        setError('Failed to fetch leaderboard');
       }
-    } catch (error) {
-      console.error('Error fetching leaderboard:', error);
-      setError('Failed to fetch leaderboard');
     }
   };
-
   const handleKeyDown = async (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
       console.log(userInput);
@@ -207,7 +221,7 @@ function App() {
 
         const headers: HeadersInit = {
           'Content-Type': 'application/json',
-          'Authorization': String(token), // Include the token in the Authorization header
+          'Authorization': `Bearer ${token}`, // Include the token in the Authorization header
         };
 
         const response = await fetch(wordApiUrl, {
@@ -216,91 +230,125 @@ function App() {
           body: JSON.stringify({ word: userInput.toLowerCase() }),
         });
 
-        if (response.ok) {
-          const responseText = await response.text();
-          console.log(responseText); // Log the response text for debugging
-          if (responseText) {
-            const data = JSON.parse(responseText);
-            console.log(data);
-            if (userInput.startsWith(curWord[curWord.length - 1])) {
-              if (wordList.includes(userInput)) {
-                //sounds.wordused.play();
-                setScore((prev) => prev - 15);
-                setError("Word already used. Try a different word!");
-                resetCombo();
-              } else {
-                const currentTime = Date.now();
-                const timeDiff = lastWordTime ? (currentTime - lastWordTime) / 1000 : 0;
+        const responseData = await response.json();
+        console.log(responseData); // Log the response data for debugging
 
-                if (comboLevel === 0 && comboCount >= 2 && timeDiff <= 10) {
-                  setComboLevel(1);
-                  setComboCount(0);
-                  setComboMessage("Combo 1 (2x Points) Activated!");
-                  //sounds.doublepoints.play();
-                } else if (comboLevel === 1 && comboCount >= 4 && timeDiff <= 15) {
-                  setComboLevel(2);
-                  setComboCount(0);
-                  setComboMessage("Combo 2 (3x Points) Activated!");
-                } else if (comboLevel === 2 && comboCount >= 9 && timeDiff <= 20) {
-                  setComboLevel(3);
-                  setComboCount(0);
-                  setComboMessage("Combo 3 (5x Points) Activated!");
-                } else if (timeDiff > 20) {
-                  resetCombo();
-                }
-
-                const comboMultiplier = comboLevel === 1 ? 2 : comboLevel === 2 ? 3 : comboLevel === 3 ? 5 : 1;
-                const points = (10 + userInput.length) * comboMultiplier;
-
-                setChainLength((prev) => prev + 1);
-                setScore((prev) => prev + points);
-                setComboCount((prev) => prev + 1);
-                setLastWordTime(currentTime);
-
-                if (userInput.length > 6) {
-                  setScore((prev) => prev + 10);
-                }
-
-                if (/[xyzj]/.test(userInput)) {
-                  setScore((prev) => prev + 5);
-                }
-
-                if (chainLength % 5 === 0) {
-                  setScore((prev) => prev + 20);
-                  //sounds.wow.play();
-                } else if (chainLength % 10 === 0) {
-                  setScore((prev) => prev + 50);
-                  //sounds.wow.play();
-                } else if (chainLength % 20 === 0) {
-                  setScore((prev) => prev + 100);
-                  //sounds.wow.play();
-                }
-
-                setCurWord(userInput);
-                setWordList((prevList) => [...prevList, userInput]);
-                setError(null);
-                setUserInput(''); // Clear the text field
-              }
-            } else {
-              //sounds.invalidword.play();
-              setError("Incorrect word. Try again!");
-              setScore((prev) => prev - 5);
+        if (responseData.statusCode === 200) {
+          if (userInput.startsWith(curWord[curWord.length - 1])) {
+            if (wordList.includes(userInput)) {
+              //sounds.wordused.play();
+              setScore((prev) => prev - 15);
+              setError("Word already used. Try a different word!");
               resetCombo();
+            } else {
+              const currentTime = Date.now();
+              const timeDiff = lastWordTime ? (currentTime - lastWordTime) / 1000 : 0;
+
+              if (comboLevel === 0 && comboCount >= 2 && timeDiff <= 10) {
+                setComboLevel(1);
+                setComboCount(0);
+                setComboMessage("Combo 1 (2x Points) Activated!");
+                //sounds.doublepoints.play();
+              } else if (comboLevel === 1 && comboCount >= 4 && timeDiff <= 15) {
+                setComboLevel(2);
+                setComboCount(0);
+                setComboMessage("Combo 2 (3x Points) Activated!");
+              } else if (comboLevel === 2 && comboCount >= 9 && timeDiff <= 20) {
+                setComboLevel(3);
+                setComboCount(0);
+                setComboMessage("Combo 3 (5x Points) Activated!");
+              } else if (timeDiff > 20) {
+                resetCombo();
+              }
+
+              const comboMultiplier = comboLevel === 1 ? 2 : comboLevel === 2 ? 3 : comboLevel === 3 ? 5 : 1;
+              const points = (10 + userInput.length) * comboMultiplier;
+
+              setChainLength((prev) => prev + 1);
+              setScore((prev) => prev + points);
+              setComboCount((prev) => prev + 1);
+              setLastWordTime(currentTime);
+
+              if (userInput.length > 6) {
+                setScore((prev) => prev + 10);
+              }
+
+              if (/[xyzj]/.test(userInput)) {
+                setScore((prev) => prev + 5);
+              }
+
+              if (chainLength % 5 === 0) {
+                setScore((prev) => prev + 20);
+                //sounds.wow.play();
+              } else if (chainLength % 10 === 0) {
+                setScore((prev) => prev + 50);
+                //sounds.wow.play();
+              } else if (chainLength % 20 === 0) {
+                setScore((prev) => prev + 100);
+                //sounds.wow.play();
+              }
+
+              setCurWord(userInput);
+              setWordList((prevList) => [...prevList, userInput]);
+              setError(null);
               setUserInput(''); // Clear the text field
             }
           } else {
-            setError("Empty response from server");
+            //sounds.invalidword.play();
+            setError("Incorrect word. Try again!");
+            setScore((prev) => prev - 5);
+            resetCombo();
+            setUserInput(''); // Clear the text field
           }
+        } else if (responseData.statusCode === 404) {
+          console.error('Error response text:', responseData.body); // Log the error response text
+          setError("Word not found. Try again!");
+          setScore((prev) => prev - 5);
+          setUserInput(''); // Clear the text field
         } else {
-          const errorText = await response.text();
-          console.error('Error response text:', errorText); // Log the error response text
+          console.error('Error response text:', responseData.body); // Log the error response text
           setError("Failed to verify word");
+          setUserInput(''); // Clear the text field
         }
       } catch (error) {
         console.error('Error verifying word:', error);
         setError('Failed to verify word');
+        setUserInput(''); // Clear the text field
       }
     }
+  };
+
+  const handleSubmitScore = async () => {
+    try {
+      const session = await fetchAuthSession();
+      const token = session.tokens?.idToken?.toString();
+      const headers: HeadersInit = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`, // Include the token in the Authorization header
+      };
+
+      const response = await fetch(LBApiUrl, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify({ email: user?.signInDetails?.loginId, Tag: userTag, score: score }),
+      });
+
+      if (response.ok) {
+        setShowSubmitForm(false);
+        await handleLeaderBoard(); // Refresh the leaderboard after submitting the score
+      } else {
+        const errorText = await response.text();
+        console.error('Error response text:', errorText); // Log the error response text
+        setError("Failed to submit score");
+      }
+    } catch (error) {
+      console.error('Error submitting score:', error);
+      setError('Failed to submit score');
+    }
+  };
+
+  const toggleInstructions = () => {
+    setShowInstructions((prev) => !prev);
   };
 
   const resetCombo = () => {
@@ -323,16 +371,38 @@ function App() {
           <button onClick={handleLeaderBoard} className="leader-button bg-gray-500 text-white py-2 px-4 rounded-lg shadow-md hover:bg-gray-700 mt-4">
             View Leaderboard
           </button>
+          <button onClick={toggleInstructions} className="how-to-play-button bg-green-500 text-white py-2 px-4 rounded-lg shadow-md hover:bg-green-700 mt-4">
+            How to Play
+          </button>
+          {showInstructions && (
+            <div className="instructions mt-4 p-4 bg-gray-100 rounded-lg shadow-md">
+              <HowToPlay />
+            </div>
+          )}
           {score > 0 && <div className="final-score text-xl font-bold mt-4">Final Score: {score}</div>}
-          <HowToPlay />
           {showLeaderboard && (
             <div className="leaderboard mt-8">
               <h3 className="text-xl font-bold mb-2">Leaderboard:</h3>
-              <ul className="list-disc list-inside">
+              <div className="leaderboard-list">
                 {leaderboard.map((entry) => (
-                  <li key={entry.id} className="text-lg">{entry.username}: {entry.score}</li>
+                  <div key={entry.id} className="text-lg">{entry.rank}. {entry.username}: {entry.score}</div>
                 ))}
-              </ul>
+              </div>
+            </div>
+          )}
+          {showSubmitForm && (
+            <div className="submit-form mt-8">
+              <h3 className="text-xl font-bold mb-2">Submit Your Score:</h3>
+              <input
+                type="text"
+                value={userTag}
+                onChange={(e) => setUserTag(e.target.value)}
+                placeholder="Enter your tag"
+                className="tag-input w-full p-2 border border-gray-300 rounded-lg mb-4"
+              />
+              <button onClick={handleSubmitScore} className="submit-button bg-green-500 text-white py-2 px-4 rounded-lg shadow-md hover:bg-green-700">
+                Submit
+              </button>
             </div>
           )}
         </div>
